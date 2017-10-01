e88d94fe3772dd8d64fe1f02c6f2542d57bfcce4
yunyuncc <wyy584322202@outlook.com>
2017-10-01 23:50:04 +0800

    实现了RPUSH LPUSH RPOP LPOP LINDEX LRANGE LTRIM

--- db/redisWrapper/redisWrapper.go
+++ db/redisWrapper/redisWrapper.go
@@ -102,6 +102,7 @@ func (cli *Client) DECR(key string) (val int64, err error) {
 	return
 }
 
+//INCRBY [Strings Group] key的val增加num
 func (cli *Client) INCRBY(key string, num int64) (val int64, err error) {
 	conn := cli.Pool.Get()
 	defer conn.Close()
@@ -109,6 +110,7 @@ func (cli *Client) INCRBY(key string, num int64) (val int64, err error) {
 	return
 }
 
+//DECRBY [Strings Group] key的val减少num
 func (cli *Client) DECRBY(key string, num int64) (val int64, err error) {
 	conn := cli.Pool.Get()
 	defer conn.Close()
@@ -116,11 +118,70 @@ func (cli *Client) DECRBY(key string, num int64) (val int64, err error) {
 	return
 }
 
+//INCRBYFLOAT [Strings Group] key的val增加num
 func (cli *Client) INCRBYFLOAT(key string, num float64) (val float64, err error) {
 	conn := cli.Pool.Get()
 	defer conn.Close()
 	val, err = redis.Float64(conn.Do("INCRBYFLOAT", key, num))
 	return
+}
+
+//APPEND [Strings Group] append val return len of new val
+func (cli *Client) APPEND(key, val string) (len int64, err error) {
+	conn := cli.Pool.Get()
+	defer conn.Close()
+	return redis.Int64(conn.Do("APPEND", key, val))
+}
+
+//RPUSH [Lists Group] 从右边放入一个值
+func (cli *Client) RPUSH(key string, val string) (len int64, err error) {
+	conn := cli.Pool.Get()
+	defer conn.Close()
+	return redis.Int64(conn.Do("RPUSH", key, val))
+}
+
+//LPUSH [Lists Group] 从左边放入一个值
+func (cli *Client) LPUSH(key string, val string) (len int64, err error) {
+	conn := cli.Pool.Get()
+	defer conn.Close()
+	return redis.Int64(conn.Do("LPUSH", key, val))
+}
+
+//RPOP [Lists Group]从右边取出一个值
+func (cli *Client) RPOP(key string) (val string, err error) {
+	conn := cli.Pool.Get()
+	defer conn.Close()
+	return redis.String(conn.Do("RPOP", key))
+}
+
+//LPOP [Lists Group] 从左边取出一个值
+func (cli *Client) LPOP(key string) (val string, err error) {
+	conn := cli.Pool.Get()
+	defer conn.Close()
+	b, err := redis.Bytes(conn.Do("LPOP", key))
+	return string(b), err
+}
+
+//LINDEX [Lists Group] 按索引取值
+func (cli *Client) LINDEX(key string, index int64) (val string, err error) {
+	conn := cli.Pool.Get()
+	defer conn.Close()
+	return redis.String(conn.Do("LINDEX", key, index))
+}
+
+//LRANGE [Lists Group] 读取start,end区间的值
+func (cli *Client) LRANGE(key string, start, end int64) (vals []string, err error) {
+	conn := cli.Pool.Get()
+	defer conn.Close()
+	return redis.Strings(conn.Do("LRANGE", key, start, end))
+}
+
+//LTRIM [Lists Group] 切除start,end之外的元素
+func (cli *Client) LTRIM(key string, start, end int64) (err error) {
+	conn := cli.Pool.Get()
+	defer conn.Close()
+	_, err = conn.Do("LTRIM", key, start, end)
+	return err
 }
 
 //DEL 删除之后再去GET会取出nil
--- db/redisWrapper/redisWrapper_test.go
+++ db/redisWrapper/redisWrapper_test.go
@@ -1,6 +1,7 @@
 package redisWrapper_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -113,4 +114,102 @@ func TestINCRDECR(t *testing.T) {
 	if err != nil {
 		t.Fatal("del fail:", err)
 	}
+}
+func TestAPPEND(t *testing.T) {
+	cli := redisWrapper.GetDefaultClient()
+	err := cli.DEL("testKey")
+	if err != nil {
+		t.Fatal("del testKey fail:", err)
+	}
+	_ = cli.SET("testKey", "val1 ")
+	len, _ := cli.APPEND("testKey", "val2")
+	if len != 9 {
+		t.Fatal("len should be 9")
+	}
+	val, _ := cli.GET("testKey")
+	if val != "val1 val2" {
+		t.Fatal("val should be val1 val2")
+	}
+	err = cli.DEL("testKey")
+	if err != nil {
+		t.Fatal("del testKey fail:", err)
+	}
+}
+
+func TestLIST(t *testing.T) {
+	cli := redisWrapper.GetDefaultClient()
+	err := cli.DEL("testList")
+	if err != nil {
+		t.Fatal("del testKey fail:", err)
+	}
+	for i := int64(1); i <= 20; i++ {
+		len, err := cli.RPUSH("testList", fmt.Sprintf("val%v", i))
+		if len != i {
+			t.Fatalf("len should be %v got:%v error:%v\n", i, len, err)
+		}
+	}
+	for i := int64(20); i >= 1; i-- {
+		val, _ := cli.RPOP("testList")
+		if val != fmt.Sprintf("val%v", i) {
+			t.Logf("should be val%v got %v", i, val)
+		}
+	}
+
+	for i := int64(1); i <= 20; i++ {
+		len, err := cli.LPUSH("testList", fmt.Sprintf("val%v", i))
+		if len != i {
+			t.Fatalf("len should be %v got:%v error:%v\n", i, len, err)
+		}
+	}
+	for i := int64(20); i >= 1; i-- {
+		val, _ := cli.LPOP("testList")
+		if val != fmt.Sprintf("val%v", i) {
+			t.Logf("should be val%v got %v", i, val)
+		}
+	}
+
+	for i := int64(1); i <= 20; i++ {
+		len, err := cli.RPUSH("testList", fmt.Sprintf("val%v", i))
+		if len != i {
+			t.Fatalf("len should be %v got:%v error:%v\n", i, len, err)
+		}
+	}
+	for i := int64(1); i <= 20; i++ {
+		val, _ := cli.LPOP("testList")
+		if val != fmt.Sprintf("val%v", i) {
+			t.Logf("should be val%v got %v", i, val)
+		}
+	}
+	cli.RPUSH("testList", "1")
+	cli.RPUSH("testList", "2")
+	cli.RPUSH("testList", "3")
+	cli.RPUSH("testList", "4")
+	val, err := cli.LINDEX("testList", 3)
+	if val != "4" {
+		t.Fatalf("should be 4 got %s, err :%v", val, err)
+	}
+	vals, err := cli.LRANGE("testList", 0, 30)
+	for i, v := range vals {
+		if v != fmt.Sprintf("%v", i+1) {
+			t.Fatalf("should be %v got %s", i, v)
+		}
+	}
+	err = cli.LTRIM("testList", 0, 3)
+	if err != nil {
+		t.Fatal("LTRIM error:%v\n", err)
+	}
+	_, err = cli.LINDEX("testList", 4)
+	if err == nil {
+		t.Fatalf("after trim index 4 should be nil\n")
+	}
+	t.Log("trim success get index 4 get err:", err)
+	val, _ = cli.LINDEX("testList", 3)
+	if val != "4" {
+		t.Fatalf("index 3 val should be 4 got %v\n", val)
+	}
+	err = cli.DEL("testList")
+	if err != nil {
+		t.Fatal("Del testList error:", err)
+	}
+
 }
